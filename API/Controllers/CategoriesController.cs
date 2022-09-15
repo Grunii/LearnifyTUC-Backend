c61ceacdc5405cbb1756e2480b8ea1bc8f31b1f1
Grunii <karl.elmgren@gmail.com>
2022-09-15 16:22:25 +0200

    Module3 Done

---
+++ API/Controllers/CategoriesController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.Dto;
+using AutoMapper;
+using Entity;
+using Entity.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class CategoriesController : BaseController
+    {
+        private readonly IGenericRepository<Category> _repository;
+        private readonly IMapper _mapper;
+
+        public CategoriesController(IGenericRepository<Category> repository, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repository = repository;
+
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<IReadOnlyList<CategoriesDto>>> GetCategories()
+        {
+            var categories = await _repository.ListAllAsync();
+            return Ok(_mapper.Map<IReadOnlyList<Category>, IReadOnlyList<CategoriesDto>>(categories));
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<CategoryDto>> GetCategory(int id)
+        {
+            var category = await _repository.GetByIdAsync(id);
+            return _mapper.Map<Category, CategoryDto>(category);
+        }
+    }
+}
