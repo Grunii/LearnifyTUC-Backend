fb113afcf39ffb7c496ae857acbc7c15e19c0bf4
Grunii <karl.elmgren@gmail.com>
2022-09-27 12:46:37 +0200

    Added Dashboard features

--- client/src/Pages/CheckoutPage.tsx
+++ client/src/Pages/CheckoutPage.tsx
@@ -6,7 +6,9 @@ import Checkout from "../Components/Checkout";
 import { setBasket } from "../redux/slice/basketSlice";
 import { useAppDispatch } from "../redux/store/configureStore";
 
-const stripePromise = loadStripe("your publishable key");
+const stripePromise = loadStripe(
+    "pk_test_51LmDhcBdXB57nezspj4T6byfPtOTCSPyHii4Ny1tz0EylXodQ3JSuMopCHhLHt18f7rUBRYMg51RZKDUvu62PEU800orcAtAyc"
+);
 
 export default function CheckoutWrapper() {
     const dispatch = useAppDispatch();
